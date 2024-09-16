eaecf6647ed43b15733731ead6c2dc33bb3d18d8
Oakley Jurgens <o.jurgens@gns.cri.nz>
2024-09-17 11:12:19 +1200

    WIP

--- src/nzshm_rupture_widget/esm/ValueButtonWidget.js
+++ src/nzshm_rupture_widget/esm/ValueButtonWidget.js
@@ -1,16 +1,16 @@
 function nextValue(value, values) {
-    const index = values.indexOf(value)
-    if ((index + 1) >= values.length) {
+    const index = values.findIndex(candidate => candidate > value)
+    if (index === -1) {
         return values[0]
     }
-    return values[index + 1]
+    return values[index]
 }
 
 function render({ model, el }) {
 
     const button = document.createElement("div")
     button.classList.add("fa")
-    button.classList.add( model.get("icon"))
+    button.classList.add(model.get("icon"))
     button.classList.add("controlButton3DMap")
 
     button.addEventListener("click", function (event) {
